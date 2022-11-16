cc74fadf0e76b5b72e7028d7bb94a31cbbab3263
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2022-11-16 10:30:10 +0200

    create sidebar, pins

--- src/container/Pins.jsx
+++ src/container/Pins.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
 
 const Pins = () => {
+    const [search, setSearch] = useState('')
+
     return (
         <div>
             Pins
